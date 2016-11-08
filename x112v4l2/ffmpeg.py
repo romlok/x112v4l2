97670821388ee75702b9fac475528eabaa11576f
Mel Collins <mel@raumkraut.net>
2016-11-08 17:45:36 +0100

    Add ffmpeg.stream and .stream_window

--- x112v4l2/ffmpeg.py
+++ x112v4l2/ffmpeg.py
@@ -36,6 +36,41 @@ def screenshot(screen_id, geometry, filename):
 	]
 	return subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 	
+def stream(screen_id, geometry, fps, filename):
+	"""
+		Streams an area of a screen to a v4l2 device node
+		
+		`screen_id` should be the full name (including display name)
+		of the X11 screen to capture. Eg: ":0.0".
+		`geometry` should be a dict-like object providing values
+		for x, y, width, and height.
+		`fps` should be the desired frames-per-second of the stream.
+		`filename` is the filesystem location where the v4l2 camera
+		device node is located.
+		
+		The return value is a subprocess.Popen instance.
+		
+		NB. All output of the ffmpeg process is devnull'ed.
+	"""
+	cmd = [
+		'ffmpeg',
+		# Need input
+		'-f', 'x11grab',
+		'-r', fps,
+		'-s', '{}x{}'.format(geometry['width'], geometry['height']),
+		'-i', '{screen}+{x},{y}'.format(
+			screen=screen_id,
+			x=geometry['x'],
+			y=geometry['y'],
+		),
+		# Output control
+		'-vcodec', 'rawvideo',
+		'-pix_fmt', 'yuv420p',
+		'-threads', '0',
+		'-f', 'v4l2', filename
+	]
+	return subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	
 
 def capture_window(window, filename):
 	"""
@@ -46,3 +81,15 @@ def capture_window(window, filename):
 		geometry=window.get_abs_geometry(),
 		filename=filename,
 	)
+	
+def stream_window(window, fps, filename):
+	"""
+		Stream the given X `window`
+	"""
+	return stream(
+		screen_id=window.screen.full_id,
+		geometry=window.get_abs_geometry(),
+		fps=fps,
+		filename=filename,
+	)
+	
