d3c41315e168baf8dbeecd4697d2bbf74b2606b8
Mel Collins <mel@raumkraut.net>
2017-01-04 13:55:59 +0100

    Make thumbs.create_all() actually generate images

--- x112v4l2/thumbs.py
+++ x112v4l2/thumbs.py
@@ -2,8 +2,13 @@
 	Functionality for dealing with our thumbnails
 """
 import os
+import time
 import tempfile
 import shutil
+import subprocess
+
+from x112v4l2 import x11
+from x112v4l2 import ffmpeg
 
 CACHE_PATH = os.path.join(tempfile.gettempdir(), 'x112v4l2', 'thumbs')
 
@@ -17,11 +22,39 @@ def rmdir():
 	return shutil.rmtree(CACHE_PATH)
 	
 
-def create_all():
+def create_all(parallel=4):
 	"""
 		Create thumbnails for all (interesting) X11 windows
 		
+		The `parallel` parameter determines how many simultaneous
+		processes will be started to create the thumbnails
+		
 		Returns a dict of {win_id: filename}
 	"""
-	return {}
+	windows = list(x11.get_windows())
+	procs = {}
+	thumbs = {}
+	while windows or procs:
+		while windows and len(procs) < parallel:
+			# Start a new process
+			window = windows.pop()
+			win_id = '{scr}.{win}'.format(
+				scr=window.screen['full_id'],
+				win=window.id,
+			)
+			filename = os.path.join(CACHE_PATH, win_id + '.png')
+			print('NEW', win_id, filename)
+			procs[win_id] = ffmpeg.capture_window(window, filename)
+			thumbs[win_id] = filename
+		
+		# Check for finished processes
+		for win_id, proc in procs.copy().items():
+			if proc.poll() is not None:
+				print('DONE', win_id)
+				procs.pop(win_id)
+			
+		# Wait a bit
+		time.sleep(0.4)
+		
+	return thumbs
 	
