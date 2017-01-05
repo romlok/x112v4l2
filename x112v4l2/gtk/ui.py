6dad1d081ffbaf6ae5026803834da62b4f70ede8
Mel Collins <mel@raumkraut.net>
2017-01-05 16:29:31 +0100

    Split signal handler into its own module

--- x112v4l2/gtk/ui.py
+++ x112v4l2/gtk/ui.py
@@ -8,10 +8,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-from x112v4l2 import v4l2
-from x112v4l2 import x11
-from x112v4l2 import ffmpeg
-from x112v4l2 import thumbs
+from x112v4l2.gtk import signals
 from x112v4l2.gtk import utils
 
 
@@ -37,8 +34,6 @@ class MainUI(object):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		
-		thumbs.mkdir()
-		
 		self.executor = futures.ProcessPoolExecutor(max_workers=self.MAX_WORKERS)
 		
 		self.load_main_window()
@@ -54,7 +49,7 @@ class MainUI(object):
 		"""
 		builder = Gtk.Builder()
 		builder.add_from_file(self.MAIN_GLADE)
-		builder.connect_signals(SignalHandler(ui=self))
+		builder.connect_signals(signals.MainHandler(ui=self))
 		# We want the main window
 		self.main_window = builder.get_object('main')
 		# We also want the device-tab widget
@@ -225,116 +220,5 @@ class MainUI(object):
 			widget.set_label(self.STATE_RELOADING_LABEL)
 		else:
 			widget.set_label(str(version))
-		
-	
-class SignalHandler(object):
-	"""
-		Handle all the signals
-	"""
-	def __init__(self, ui, **kwargs):
-		"""
-			Create a new SignalHandler
-			
-			The `ui` parameter should be an instance of a MainUI class.
-		"""
-		super().__init__(**kwargs)
-		self.ui = ui
-		
-	
-	def exit_application(self, *args):
-		return Gtk.main_quit(*args)
-		
-	
-	def on_show_main(self, *args):
-		"""
-			Triggered when the main window is shown
-		"""
-		self.refresh_v4l2_info()
-		self.refresh_ffmpeg_info()
-		self.refresh_x11_info()
-		self.regen_x11_thumbs()
-	
-	def refresh_v4l2_info(self, *args):
-		"""
-			Recheck the state of the v4l2loopback kernel module
-		"""
-		# Indicate that stuff is reloading
-		self.ui.show_v4l2_available(self.ui.STATE_RELOADING)
-		self.ui.show_v4l2_loaded(self.ui.STATE_RELOADING)
-		self.ui.show_v4l2_devices(self.ui.STATE_RELOADING)
-		
-		# Async info-getting
-		avail_future = self.ui.executor.submit(v4l2.get_module_available)
-		avail_future.add_done_callback(
-			lambda f: self.ui.show_v4l2_available(f.result())
-		)
-		
-		loaded_future = self.ui.executor.submit(v4l2.get_module_loaded)
-		loaded_future.add_done_callback(
-			lambda f: self.ui.show_v4l2_loaded(f.result())
-		)
-		
-		devices_future = self.ui.executor.submit(v4l2.get_devices)
-		devices_future.add_done_callback(
-			lambda f: self.ui.show_v4l2_devices(f.result())
-		)
-		
-	def set_v4l2_device_info(self, *args):
-		"""
-			Applies any changes made to the v4l2 configuration
-		"""
-		## TODO: Indicate we're updating
-		names = self.ui.get_device_names()
-		devices_future = self.ui.executor.submit(v4l2.configure_devices, names)
-		devices_future.add_done_callback(
-			lambda f: self.refresh_v4l2_info(f.result())
-		)
-		
-	
-	def refresh_x11_info(self, *args):
-		"""
-			Recheck the X11 situation
-		"""
-		# Indicate stuff is reloading
-		self.ui.show_x11_display_info(self.ui.STATE_RELOADING)
-		self.ui.show_x11_screen_info(self.ui.STATE_RELOADING)
-		self.ui.show_x11_window_info(self.ui.STATE_RELOADING)
-		
-		# Xlib classes aren't picklable, so we can't future this :/
-		displays = x11.get_displays()
-		self.ui.show_x11_display_info(displays)
-		screens = x11.get_screens()
-		self.ui.show_x11_screen_info(screens)
-		windows = x11.get_windows()
-		self.ui.show_x11_window_info(list(windows))
-		
-	def regen_x11_thumbs(self, *args):
-		"""
-			(Re-)Generate thumbnails of all X11 windows
-		"""
-		self.ui.show_x11_thumb_path(thumbs.CACHE_PATH)
-		self.ui.show_x11_thumbs(self.ui.STATE_RELOADING)
-		
-		future = self.ui.executor.submit(thumbs.create_all)
-		future.add_done_callback(
-			lambda f: self.ui.show_x11_thumbs(f.result())
-		)
-		
-	
-	def refresh_ffmpeg_info(self, *args):
-		"""
-			Recheck the state of the ffmpeg binary
-		"""
-		# Indicate that stuff is reloading
-		self.ui.show_ffmpeg_installed(self.ui.STATE_RELOADING)
-		self.ui.show_ffmpeg_version(self.ui.STATE_RELOADING)
-		
-		version_future = self.ui.executor.submit(ffmpeg.get_version)
-		version_future.add_done_callback(
-			lambda f: self.ui.show_ffmpeg_installed(f.result())
-		)
-		version_future.add_done_callback(
-			lambda f: self.ui.show_ffmpeg_version(f.result())
-		)
 		
 	
