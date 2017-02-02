576c0e6bd3694cbacf8c74047bfea4a95a943cc6
Mel Collins <mel@raumkraut.net>
2017-02-02 15:59:05 +0100

    Have v4l2-reload wait if /dev/video0 is unreadable

--- v4l2-reload.sh
+++ v4l2-reload.sh
@@ -9,3 +9,10 @@ set -e
 
 /sbin/modprobe -r v4l2loopback
 /sbin/modprobe v4l2loopback "$@"
+
+# The very first time the module is installed after boot,
+# it appears that the permissions on /dev/video0 may not
+# be set correctly until a short time has passed.
+if [ -r /dev/video0 ]; then
+	sleep 0.5
+fi
